9be2e4d81deec121e04e199e68141299f7a30f4e
LJS0000 <leejisu2281@gmail.com>
2024-11-20 16:25:40 +0900

    Bug: AuthForm 로그인 시 에러 발생

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,7 +5,8 @@ const LoginPage = () => {
   return (
     <div>
       <h1>LoginPage</h1>
-      <AuthForm type="login" />
+      {/* 버그 발견 */}
+      {/* <AuthForm type="login" /> */}
       <Link to="/signup">회원가입</Link>
     </div>
   );
