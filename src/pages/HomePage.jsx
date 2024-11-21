ec0183cb937868e5fc9fa312a6d959bb617be1d3
LJS0000 <leejisu2281@gmail.com>
2024-11-21 15:50:02 +0900

    Feat: mockdata 그리기

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,17 @@
-import Modal from "../components/Modal";
+import { mockContents } from "../constants/mockContents";
 import Category from "../components/Category";
+import ContentCard from "../components/ContentCard";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <div>
-        <Modal />
+    <main>
+      <Category />
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {mockContents.map((content) => (
+          <ContentCard content={content} key={content.id} />
+        ))}
       </div>
-      <div>
-        <Category />
-      </div>
-    </div>
+    </main>
   );
 };
 
